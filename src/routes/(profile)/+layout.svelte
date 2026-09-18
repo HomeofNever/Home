0ce40dabc2511bcbd9f4c97d3ed7ac97b17beb2e
NeverBehave <git@n4b5.org>
2026-09-17 23:21:52 -0700

    fix(profile): restore desktop greeting

--- src/routes/(profile)/+layout.svelte
+++ src/routes/(profile)/+layout.svelte
@@ -8,12 +8,12 @@
 <svelte:head>
   <meta property="og:title" content="About NeverBehave" />
   <meta property="og:url" content="https://never.pet" />
-  <meta property="og:description" content="Glad our paths crossed :3" />
+  <meta property="og:description" content="欸，是咕咕喵" />
   <meta property="og:image" content="{base}/assets/images/current.png" />
   <meta name="twitter:card" content="summary" />
   <meta name="twitter:title" content="About NeverBehave" />
   <meta name="twitter:site" content="@_NeverBehave_" />
-  <meta name="twitter:description" content="Glad our paths crossed :3" />
+  <meta name="twitter:description" content="欸，是咕咕喵" />
   <meta name="twitter:image" content="{base}/assets/images/current.png" />
   <title>About NeverBehave</title>
 </svelte:head>
